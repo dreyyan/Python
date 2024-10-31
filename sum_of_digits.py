9946ff21b09b652b1ae434d0221a062f1bfa5c5d
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-10-31 22:52:39 +0800

    Add files via upload

---
+++ sum_of_digits.py
@@ -0,0 +1,10 @@
+print("Sum of Digits")
+
+number = input("Enter an Integer: ")
+
+sum_of_digits = 0
+
+for char in str(number):
+    sum += int(char)
+
+print("Sum of Digits: " + str(sum_of_digits))
