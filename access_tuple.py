f1f41ec500485fb8c1d8350bb192ec800c8b8b2c
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-06 18:57:04 +0800

    Add files via upload

---
+++ access_tuple.py
@@ -0,0 +1,12 @@
+def access_tuple():
+    string = "Apple"
+    int = 19
+    isBool = True
+    float = 20.5
+    new_tuple = (string, int, isBool, float)
+    print(new_tuple[0])
+    print(new_tuple[1])
+    print(new_tuple[2])
+    print(new_tuple[3])
+
+access_tuple()
