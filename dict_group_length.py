7be65a842fb404f9a73a2ad36b59c17b429fffcd
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-08 11:03:56 +0800

    Add files via upload

--- dict_group_length.py
+++ dict_group_length.py
@@ -1,7 +1,13 @@
 def group_length(t1):
     dict = {}
+    length = 0
+    letter_count = 0
     for i in t1:
-
+        length = len(i)
+        if length not in dict:
+            dict[length] = [i]
+        else:
+            dict[length].append(i)
     print(dict)
-test_list = {'cat', 'ocean', 'dog', 'river', 'goat'}
+test_list = ['cat', 'ocean', 'dog', 'river', 'goat']
 group_length(test_list)
