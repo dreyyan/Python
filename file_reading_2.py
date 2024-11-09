17df343399145b1b3cabb593ee1bf2487e913490
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-09 09:27:38 +0800

    file_reading_2

--- file_reading_2.py
+++ file_reading_2.py
@@ -1,6 +1,5 @@
 with open('.txt/test.txt', 'r') as f:
     size_to_read = 20
     read_content = f.read(size_to_read)
-    print(read_content, end='')
+    print(read_content, end='') # End with blank instead of newline
     f.seek(0) # Redirects back to first character
-# f.close() # No need to f.close() when using 'with'
